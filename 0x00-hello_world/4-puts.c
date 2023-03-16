4b7f378c3ed47e50c44246190982656104533fd3
mouadia <ouadia.mohammed@outlook.de>
2023-03-16 19:50:16 +0100

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main Function
+ *
+ * Description: this program prints a string using the put function.
+ * Return: Always 0.
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
