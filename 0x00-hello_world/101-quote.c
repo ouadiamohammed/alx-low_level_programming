3a334ab27cace2c093c63590d6daa00236cb78b6
mouadia <ouadia.mohammed@outlook.de>
2023-03-16 21:08:42 +0100

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int	main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int len = sizeof(str) - 1;
 
 	write(2, &str, len);
