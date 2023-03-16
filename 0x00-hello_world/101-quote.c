4840c2e623c81188947207d780c1326002778d34
mouadia <ouadia.mohammed@outlook.de>
2023-03-16 21:07:28 +0100

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: this program prints a String using the Function write.
+ * Return: it returns 1.
+ */
+int	main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
+	int len = sizeof(str) - 1;
+
+	write(2, &str, len);
+	return (1);
+}
