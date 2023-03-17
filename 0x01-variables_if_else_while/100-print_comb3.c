cb373dcce1c7992dbfdb90a327c9961bff8a70c1
mouadia <ouadia.mohammed@outlook.de>
2023-03-17 22:58:01 +0100

    comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,14 @@ int	main(void)
 	int a = 48;
 	int b = 48;
 
-	while(a < 58)
+	while (a < 58)
 	{
 		b = a + 1;
-		while(b < 58)
+		while (b < 58)
 		{
 			putchar(a);
 			putchar(b);
-			if( a < 56 || b < 57)
+			if (a < 56 || b < 57)
 			{
 				putchar(44);
 				putchar(32);
