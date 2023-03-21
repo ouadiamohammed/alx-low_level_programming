c39450e7a8ac4cdda0d0bff26bdadeaff5858d8f
mouadia <ouadia.mohammed@outlook.de>
2023-03-21 20:56:56 +0100

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - read description.
+ *
+ * @n: is a giving integer.
+ * Description: this function prints the signe of a number.
+ *
+ * Return: 1 for positive, 0 for 0 and, -1 for negative
+ */
+
+int	print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
