d05866bcf37b9a3e584b50b3d6ef10fc54fe0845
mouadia <ouadia.mohammed@outlook.de>
2023-03-21 21:31:05 +0100

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,7 +15,12 @@ int	print_last_digit(int	n)
 	int ld = n % 10;
 
 	if (ld >= 0)
+	{
+		_putchar(ld + '0');
 		return (ld);
+	}
 	else
-		return (ld * (-1));
+	{
+		_putchar(-ld + '0');
+		return (-ld);
 }
