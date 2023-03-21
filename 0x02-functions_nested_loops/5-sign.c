51f319956eaae51c0b0188e3e4402a711f5db48c
mouadia <ouadia.mohammed@outlook.de>
2023-03-21 21:07:26 +0100

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,14 +16,12 @@ int	print_sign(int n)
 		_putchar(43);
 		return (1);
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	if (n < 0)
-	{
+	else
 		_putchar(45);
-		return (-1);
-	}
+	return (-1);
 }
