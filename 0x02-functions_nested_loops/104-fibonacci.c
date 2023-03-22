4fc776977007716b98c91c75c4ecfd76e5ae3b18
mouadia <ouadia.mohammed@outlook.de>
2023-03-22 01:32:55 +0100

    fibo

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - read description
+ *
+ * Description: this program that finds and prints
+ *              the first 98 Fibonacci numbers.
+ *
+ * Return: 0.
+ */
+
+int	main(void)
+{
+	int a = 1,
+	int b = 2;
+	int sum = 0;
+	int i = 0;
+
+	printf("%d, %d, ", a, b);
+	for (i = 2; i < 98; i++)
+	{
+		sum = a + b;
+		printf("%d", sum);
+		if (i < 97)
+			printf(", ");
+		a = b;
+		b = sum;
+	}
+	printf("\n);
+	return (0);
+}
