3cab111dbd93e6733d8f10b45aafb4c1e8954695
mouadia <ouadia.mohammed@outlook.de>
2023-03-27 18:09:13 +0200

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,25 +11,24 @@
 int	_atoi(char *s)
 {
 	int i = 0;
-	int sign = 1;
+	int sign = -1;
 	int result = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-		{
 			sign *= -1;
-			i++;
-		}
-		else
-			i++;
 
-		while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			result *= 10;
-			result += s[i] - 48;
-			i++;
+			if (result < 0)
+				result = (result * 10) - (s[i] - '0');
+			else
+				result = (s[i] - '0') * -1;
+			if (s[i+1] < 48 || s[i+1] > 57)
+				break;
 		}
+		i++;
 	}
 	return (result * sign);
 }
