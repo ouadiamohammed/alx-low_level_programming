6c37c365025312dcab8ce0652654cffb34fd755c
mouadia <ouadia.mohammed@outlook.de>
2023-03-29 22:57:05 +0200

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,23 +8,21 @@
  * Return: string.
  */
 
-char	*cap_string(char *str)
+char *cap_string(char *str)
 {
-	int i = 0;
+	int symb[14] = {' ', '\t', '\n', ',', ';', '.', '!',
+		'?', '"', '(', ')', '{', '}'};
+	int i, j;
 
-	if (str[0] >= 'a' && str[0] <= 'z')
-		str[0] -= 32;
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i - 1] == ' ' || str[i - 1] == '\n' || str[i - 1] == ','
-			|| str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!'
-			|| str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] ==  '('
-			|| str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] -= 32;
-		}
-		i++;
+		if (str[0] >= 97 && str[0] <= 122)
+	{
+			str[0] = str[0] - 32;
+	}
+	for (j = 0; j < 14; j++)
+		if (str[i] >= 97 && str[i] <= 122 && str[i - 1] == symb[j])
+			str[i] = str[i] - 32;
 	}
 	return (str);
 }
