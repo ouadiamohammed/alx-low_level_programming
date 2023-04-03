1e522fc944a384821a3981a91ba084ed1d491417
mouadia <ouadia.mohammed@outlook.de>
2023-04-03 16:12:59 +0200

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard.
+ *
+ * @a: 2D array.
+ *
+ * Return: void.
+ */
+
+void	print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int j = 0;
+
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j];
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
