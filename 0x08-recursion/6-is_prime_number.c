b8f9a148a4b1a7ef0157f2b1e0ecb04501640438
mouadia <ouadia.mohammed@outlook.de>
2023-04-05 23:26:13 +0200

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * check_prime - check if num can be divided or not.
+ * @n: number to be ckecked.
+ * @div: divisor.
+ * Return: 1 if prime, 0 if not, or recursive function call.
+ */
+
+int	check_prime(int n, int div)
+{
+	if (n == div)
+		return (1);
+	if (n % div == 0)
+		return (0);
+	return (check_prime(n, div + 1));
+}
+
+/**
  * is_prime_number - returns 1 if the input is prime number,
  *                   otherwise return 0.
  * @n: giving number.
@@ -9,15 +25,12 @@
 
 int is_prime_number(int n)
 {
-	int i = 2;
+	int div = 3;
 
-	if (n <= 1)
+	if (n % 2 == 0 || n < 2)
 		return (0);
-	while (i <= n/i)
-	{
-		if (n % i == 0)
-			return (0);
-		i++;
-	}
-	return (1);
+	if (n == 2)
+		return (1);
+
+	return (check_prime(n, div));
 }
