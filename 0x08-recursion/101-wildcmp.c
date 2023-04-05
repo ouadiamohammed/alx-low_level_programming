70761f6954f10ba305c598b304691fd82b96bcb1
mouadia <ouadia.mohammed@outlook.de>
2023-04-06 01:19:19 +0200

    wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * str_check - checks if strings are identical.
+ * @s1: first string.
+ * @s2: second string.
+ * @i: index.
+ * @j: index.
+ * Return: 1 if string is palindrome, 0 if not.
+ */
+
+int	str_check(char *s1, char *s2, int i, int j)
+{
+	if (s1[i] == '\0' && s2[j] == '\0')
+		return (1);
+	if (s1[i] == s2[j])
+		return (str_check(s1, s2, i + 1, j + 1));
+	if (s1[i] == '\0' && s2[j] == '*')
+		return (str_check(s1, s2, i, k + 1));
+	if (s2[k] == '*')
+		return (str_check(s1, s2, i + 1, k) || str_check(s1, s2, i, k + 1));
+	return (0);
+}
+
+/**
+ * wildcmp - checks if strings are indentical.
+ * @s1: string.
+ * @s2: string.
+ * Return: 1 if strings are considered identical.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	return (str_check(s1, s2, 0, 0));
+}
