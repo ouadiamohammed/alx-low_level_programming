18edded21a4c2a52596d3ac067040198889c5c4b
mouadia <ouadia.mohammed@outlook.de>
2023-04-09 13:18:04 +0200

    mult

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers.
+ *
+ * @argc: counter.
+ * @argv: array of pointers to arguments.
+ *
+ * Return: 0 for succes and 1 for error.
+ */
+
+int main(int argc, char *argv[])
+{
+	int result = 0;
+
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+	else
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+	}
+	return (0);
+}
