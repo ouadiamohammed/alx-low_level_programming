afd4529b2cc91bc099d2f577a8072b54eb9aea35
mouadia <ouadia.mohammed@outlook.de>
2023-04-13 22:52:57 +0200

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,6 +2,23 @@
 #include "main.h"
 
 /**
+ * _strlen - gives the length of a giving string.
+ *
+ * @str: pointer to a giving string.
+ *
+ * Return: string length.
+ */
+
+int	_strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
  * string_nconcat - allocates memory for an array.
  *
  * @s1: pointer to first string.
