5b333765663b57272d9d1adabc510e95e4213ecc
mouadia <ouadia.mohammed@outlook.de>
2023-04-20 00:01:20 +0200

    calc

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,7 +21,8 @@ int (*get_op_func(char *s))(int, int)
 			{NULL, NULL}
 	};
 	int i = 0;
-	while (ops[i].op != NULL)
+
+	while (i < 5)
 	{
 		if (s[0] == ops[i].op[0] && s && !s[1])
 			return (ops[i].f);
