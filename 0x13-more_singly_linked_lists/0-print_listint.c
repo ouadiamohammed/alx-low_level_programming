ef366457dc6bda5f8357d266a925531aa5c418ff
mouadia <ouadia.mohammed@outlook.de>
2023-04-29 14:38:52 +0200

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 /**
  * print_listint - prints all the elements of a listint_t list.
  * @h: input list.
- * @Return: counter(the number of nodes).
+ * Return: counter(the number of nodes).
  */
 
 size_t print_listint(const listint_t *h)
