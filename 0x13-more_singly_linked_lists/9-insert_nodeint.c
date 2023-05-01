9a7dca584595d04e890fe73b2c00e9b105961719
mouadia <ouadia.mohammed@outlook.de>
2023-05-01 13:24:25 +0200

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL)
 		return (NULL);
-	new_node = malloc(sizeof(listint));
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
@@ -34,6 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		}
 		node = node->next;
+		i++;
 	}
 	new_node->next = node->next;
 	node->next = new_node;
