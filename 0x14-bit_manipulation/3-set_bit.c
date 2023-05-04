ddb218e3240d5551eb75f923dcc725f18d920ec6
mouadia <ouadia.mohammed@outlook.de>
2023-05-05 00:02:28 +0200

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,9 +9,11 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int num = 1;
-
+	unsigned long int number = 1;
 	if (index > 63)
 		return (-1);
-	return ((n >> index) & 1);
+	number <<= index;
+	*n = *n | number;
+
+	return (1);
 }
