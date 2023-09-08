574ac61a5583bb0ff21b9fc333f7b36fb0e9eb59
mouadia <ouadia.mohammed@outlook.de>
2023-09-08 20:41:55 +0200

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_ht = NULL;
 	unsigned long int i = 0;
 
+	if (size == 0)
+		return (NULL);
+
 	new_ht = malloc(sizeof(hash_node_t));
 	if (!new_ht)
 		return (NULL);
