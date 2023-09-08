97bf0c7ba2c4e57b01c69d686f0a20a6ac0b4d81
mouadia <ouadia.mohammed@outlook.de>
2023-09-08 20:27:47 +0200

    hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,11 +10,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_ht;
 	unsigned long int i = 0;
 
-	if (size == NULL)
+	if (!size)
 		return (NULL);
 
 	new_ht = malloc(sizeof(hash_node_t));
-	if (new_ht == NULL)
+	if (!new_ht)
 		return (NULL);
 
 	new_ht->size = size;
