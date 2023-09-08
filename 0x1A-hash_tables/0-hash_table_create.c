4e3d88c35213ea91a8e54f01e040d266e3a89512
mouadia <ouadia.mohammed@outlook.de>
2023-09-08 20:20:50 +0200

    hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: the size of the array
+ * Return: pointer to the created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_ht;
+	unsigned long int i = 0;
+
+	if (size == NULL)
+		return (NULL);
+
+	new_ht = malloc(sizeof(hash_node_t));
+	if (new_ht == NULL)
+		return (NULL);
+
+	new_ht->size = size;
+	new_ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (!new_ht->array)
+	{
+		free(new_ht);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		new_ht->array[i] = NULL;
+	}
+	return (new_ht);
+}
