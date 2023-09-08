b318fb03f7e41ba3ba3d4dbcdf44df8859603ab4
mouadia <ouadia.mohammed@outlook.de>
2023-09-08 22:20:00 +0200

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,7 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
-		return (NULL);
+		return (0);
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
