eaaad067a2f1474f5047f8fb3a700012ff1485aa
mouadia <ouadia.mohammed@outlook.de>
2023-09-08 23:59:11 +0200

    ex06

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -14,19 +14,16 @@ void hash_table_delete(hash_table_t *ht)
 
 	if (!ht)
 		return;
-	for (; i < ht->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
+		node1 = ht->array[i];
+		while (node1 != NULL)
 		{
-			node1 = ht->array[i];
-			while (node1)
-			{
-				node2 = node1->next;
-				free(node1->key);
-				free(node1->key);
-				free(node1->value);
-				node1 = node2;
-			}
+			node2 = node1->next;
+			free(node1->key);
+			free(node1->value);
+			free(node1;
+			node1 = node2;
 		}
 	}
 	free(ht->array);
